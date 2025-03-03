582d6e250a498791bc2f361bc0fd8b442baf95ff
Dominik <mrdominino@gmail.com>
2025-03-03 15:18:20 +0100

    feat: add include_protocol parameter to front url resolver

--- bug_tracker/core/url_resolver.py
+++ bug_tracker/core/url_resolver.py
@@ -9,10 +9,10 @@ class FrontendUrlType(str, Enum):
     VERIFY_EMAIL = "VERIFY_EMAIL"
 
 
-def resolve_front_url(url_type: FrontendUrlType | str) -> str:
+def resolve_front_url(url_type: FrontendUrlType | str, include_protocol: bool = True) -> str:
     if url_type not in settings.FRONTEND_URLS:
         raise ValueError(f"{url_type} is not defined in settings.FRONTEND_URLS")
-    url = settings.FRONTEND_URLS[url_type]
+    url = f"{'https://' if include_protocol else ''}{settings.FRONTEND_URLS[url_type]}"
     if url_type == FrontendUrlType.BASE:
         return url
     return f"{settings.FRONTEND_URLS[FrontendUrlType.BASE]}/{url}"
