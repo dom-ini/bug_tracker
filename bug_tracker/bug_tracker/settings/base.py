145d0cf4207d4291bb6cf2fe22c30dd5e2764d06
Dominik <mrdominino@gmail.com>
2025-03-16 18:58:28 +0100

    feat: change default pagination to limit-offset

--- bug_tracker/bug_tracker/settings/base.py
+++ bug_tracker/bug_tracker/settings/base.py
@@ -204,8 +204,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.ScopedRateThrottle",
         "rest_framework.throttling.AnonRateThrottle",
