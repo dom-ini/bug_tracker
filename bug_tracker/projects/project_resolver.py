21c2abf522cd3bc24b8ac5573d571f9a94e04827
Dominik <mrdominino@gmail.com>
2025-03-16 19:08:31 +0100

    refactor: use selector in project resolver

--- bug_tracker/projects/project_resolver.py
+++ bug_tracker/projects/project_resolver.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 from projects.models import Project
+from projects.selectors.project import project_get_by_subdomain
 
 
 class MissingProjectIdentifierHeader(Exception):
@@ -12,9 +13,6 @@ def resolve_project_from_header(request: HttpRequest) -> Project | None:
     header_name = settings.SUBDOMAIN_HEADER_NAME
     subdomain = request.headers.get(header_name)
     if not subdomain:
-        raise MissingProjectIdentifierHeader(_(f"Missing {header_name} header"))
+        raise MissingProjectIdentifierHeader(_("Missing %(header_name)s header") % {"header_name": header_name})
 
-    try:
-        return Project.objects.get_by_subdomain(subdomain)
-    except Project.DoesNotExist:
-        return None
+    return project_get_by_subdomain(subdomain=subdomain, user=request.user)
