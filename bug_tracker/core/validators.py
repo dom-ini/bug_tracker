c69fb78f98a1c26310b5a5cdfb5866731fb2d212
Dominik <mrdominino@gmail.com>
2025-04-23 20:32:44 +0200

    refactor: extract mime type checking to a separate function in file type validator

--- bug_tracker/core/validators.py
+++ bug_tracker/core/validators.py
@@ -9,12 +9,12 @@ from django.utils.translation import gettext_lazy as _
 default_extension_validator = FileExtensionValidator(allowed_extensions=settings.ATTACHMENTS_ALLOWED_FILE_TYPES.keys())
 
 
-def validate_file_type(file: File, extension_validator=default_extension_validator) -> None:
-    extension_validator(file)
-
+def validate_mime_type(
+    file: File, allowed_file_types: dict[str, str] = settings.ATTACHMENTS_ALLOWED_FILE_TYPES
+) -> None:
     mime_type = magic.Magic(mime=True).from_buffer(file.read(2048))
     ext = get_file_extension(file.name)
-    expected_mime_type = settings.ATTACHMENTS_ALLOWED_FILE_TYPES.get(ext)
+    expected_mime_type = allowed_file_types.get(ext)
 
     if expected_mime_type and mime_type != expected_mime_type:
         raise ValidationError(
@@ -23,8 +23,14 @@ def validate_file_type(file: File, extension_validator=default_extension_validat
         )
 
 
-def validate_file_size(file: File) -> None:
-    max_size = settings.ATTACHMENTS_MAX_SIZE
+def validate_file_type(
+    file: File, extension_validator=default_extension_validator, mime_type_validator=validate_mime_type
+) -> None:
+    extension_validator(file)
+    mime_type_validator(file)
+
+
+def validate_file_size(file: File, max_size: int = settings.ATTACHMENTS_MAX_SIZE) -> None:
     if file.size > max_size:
         raise ValidationError(
             _("File should not exceed %(max_size)s MiB.") % {"max_size": f"{bytes_to_mib(max_size):.6f}"}
