453e66b2382b8d4f03a2254ff275f7c839a0661f
Dominik <mrdominino@gmail.com>
2025-04-25 14:40:56 +0200

    feat: change custom auth views names to not clash with dj_rest_auth

--- bug_tracker/users/urls.py
+++ bug_tracker/users/urls.py
@@ -3,10 +3,10 @@ from django.views.generic import TemplateView
 from users.views import CustomPasswordResetView, CustomRegisterView, CustomResendEmailVerificationView
 
 urlpatterns = [
-    path("password/reset/", CustomPasswordResetView.as_view(), name="rest_password_reset"),
+    path("password/reset/", CustomPasswordResetView.as_view(), name="password_reset"),
     path("", include("dj_rest_auth.urls")),
-    path("registration/", CustomRegisterView.as_view(), name="rest_register"),
-    path("registration/resend-email/", CustomResendEmailVerificationView.as_view(), name="rest_resend_email"),
+    path("registration/", CustomRegisterView.as_view(), name="register"),
+    path("registration/resend-email/", CustomResendEmailVerificationView.as_view(), name="resend_email"),
     path("registration/", include("dj_rest_auth.registration.urls")),
     path(
         "password-reset/confirm/<uidb64>/<token>/",
